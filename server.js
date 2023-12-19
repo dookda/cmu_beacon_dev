692c6a2966d62a560a00688d1a06ee7fa619e011
dookda <sakda.homhuan@gmail.com>
2023-12-19 16:08:06 +0700

    update init nodejs express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const api = require('./service/api');
+app.use(api);
+
+app.use('/', express.static('www'));
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
